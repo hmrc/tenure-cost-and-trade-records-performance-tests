3242491ddc35f5117dcf2f0db359b241ee6b36c5
snehaprathipat1 <11062150+snehaprathipat1@users.noreply.github.com>
2023-11-08 17:17:15 +0000

    wip

--- src/test/scala/uk/gov/hmrc/perftests/tctr/requests/FranchiseOrLettingsRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/tctr/requests/FranchiseOrLettingsRequests.scala
@@ -73,6 +73,7 @@ object FranchiseOrLettingsRequests extends HttpConfiguration with servicesConfig
   def getCateringOperationRent(index: Int): HttpRequestBuilder =
     http("[GET] get catering operation rent page")
       .get(s"$baseUrl/$route/catering-operation-rent?idx=$index")
+      .queryParam("idx", index)
       .check(status.is(200))
       .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
 
